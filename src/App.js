0372369535da7111ea7688f8ebf18b065a757ade
Bgstatic <bilgehangecici@gmail.com>
2020-09-05 18:22:33 +0300

    Spotify web api and state management (Context Api) integration.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
-import React from 'react';
-import './styles/App.css';
+import React, { useEffect } from 'react';
+import { getTokenFromUrl } from './components/Spotify';
 import Login from './components/Login';
+import Player from './components/Player';
+import { useDataLayerValue } from './components/DataLayer';
+import './styles/App.css';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+
+  const [{ user, token }, dispatch] = useDataLayerValue();
+
+  useEffect(() => {
+
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+
+      dispatch({
+        type: 'SET_TOKEN',
+        token: _token,
+
+      });
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user => {
+
+        dispatch({
+          type: 'SET_USER',
+          user,
+        });
+
+      });
+    }
+
+  }, [dispatch]);
+
   return (
     <div className="app">
-      <Login/>
+      {
+        token ? (
+
+          <Player spotify= {spotify}/>
+
+        ) : (
+
+            <Login />
+          )
+      }
+
     </div>
   );
 }
