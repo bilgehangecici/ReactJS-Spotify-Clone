fefbf1eb5217c327053d9b6b9a624ace8e9a5d6c
Bgstatic <bilgehangecici@gmail.com>
2020-09-05 18:27:11 +0300

    Login.js updated.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import '../styles/Login.css';
-import {loginUrl} from './Spotify';
+import { loginUrl } from './Spotify';
 
 function Login() {
+
     return (
         <div className="login">
             <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="spotify" />
