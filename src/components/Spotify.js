0b201e1758f5d014b764e15ea662c1a12f0bcfac
Bgstatic <bilgehangecici@gmail.com>
2020-09-05 15:55:54 +0300

    Login and User Authentication landed.

---
+++ src/components/Spotify.js
@@ -0,0 +1,20 @@
+// https://developer.spotify.com/documentation/web-playback-sdk/quick-start/
+
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+const redirectUri = "http://localhost:3000/";
+
+const clientId = "ad9f9667aaa54acda274e5698653f05a";
+
+const scopes = [
+
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+
+];
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=
+${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`
