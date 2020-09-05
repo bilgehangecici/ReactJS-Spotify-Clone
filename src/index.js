a5e0bb2340c6ec8456181c7f390ddfab64856486
Bgstatic <bilgehangecici@gmail.com>
2020-09-05 18:26:45 +0300

    Context Api integration.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
+import { DataLayer } from './components/DataLayer';
+import reducer, { initialState } from './components/Reducer';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DataLayer initialState={initialState} reducer={reducer}>
+      <App />
+    </DataLayer>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
