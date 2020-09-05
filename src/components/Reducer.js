46957ec3b2d4cd6b5352628952795d6d75035e7c
Bgstatic <bilgehangecici@gmail.com>
2020-09-05 18:25:11 +0300

    Created Reducer.js for setState operations (State Management).

---
+++ src/components/Reducer.js
@@ -0,0 +1,34 @@
+export const initialState = {
+
+    user: null,
+    playlist: [],
+    playing: false,
+    item: null,
+    token: null,
+}
+
+const reducer = (state, action) => {
+
+    console.log(action);
+
+    switch (action.type) {
+
+        case 'SET_USER':
+            return {
+
+                ...state,
+                user: action.user,
+            }
+        case 'SET_TOKEN':
+            return {
+
+                ...state,
+                token: action.token,
+            }
+        default:
+            return state;
+    }
+
+}
+
+export default reducer;
