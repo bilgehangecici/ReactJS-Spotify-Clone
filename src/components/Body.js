2ab07fcf456aff9c6749e6f02170bfef2a5ab875
Bgstatic <bilgehangecici@gmail.com>
2020-09-06 14:07:19 +0300

    Body.js for listing musics and playlists.

---
+++ src/components/Body.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Body.css';
+
+function Body() {
+    return (
+        <div className="body">
+            <h1>Body here</h1>
+        </div>
+    )
+}
+
+export default Body
