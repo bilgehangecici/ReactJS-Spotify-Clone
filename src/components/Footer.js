98089d5179874865e5e7b35bf27e4b754fe83b54
Bgstatic <bilgehangecici@gmail.com>
2020-09-06 15:30:51 +0300

    Added Spotify buttons (play, skip-next, previous, shuffle) with material-ui.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,50 @@
 import React from 'react'
 import '../styles/Footer.css';
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import RepeatIcon from '@material-ui/icons/Repeat';
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+import { Grid, Slider } from "@material-ui/core";
 
 function Footer() {
     return (
         <div className="footer">
-            <h1>Footer here</h1>
+            <div className="footer__left">
+             <img src="" className="footer__albumLogo"   alt=""/>
+             <div className="footer__songInfo">
+                 <h4>No song is playing</h4>
+                 <p>...</p>
+             </div>
+            </div>
+
+            <div className="footer__center">
+
+                <ShuffleIcon className="footer__green" />
+                <SkipPreviousIcon className="footer__icon" />
+                <PlayCircleOutlineIcon fontSize="large" className="footer__icon" />
+                <SkipNextIcon className="footer__icon" />
+                <RepeatIcon className="footer__green" />
+
+            </div>
+
+            <div className="footer__right">
+                <Grid container spacing={2}>
+                    <Grid item>
+                        <PlaylistPlayIcon />
+                    </Grid>
+                    <Grid item>
+                        <VolumeDownIcon />
+                    </Grid>
+                    <Grid item xs>
+                        <Slider aria-labelledby="continuous-slider" />
+                    </Grid>
+                </Grid>
+
+            </div>
         </div>
     )
 }
