09688ace3452a036df2635b0155369ae4aaf4020
Bgstatic <bilgehangecici@gmail.com>
2020-09-06 14:08:02 +0300

    Footer.js for spotify music player.

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Footer.css';
+
+function Footer() {
+    return (
+        <div className="footer">
+            <h1>Footer here</h1>
+        </div>
+    )
+}
+
+export default Footer
