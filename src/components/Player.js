e5d67bfc4fbb7ea60d86e8fb02ddcc609cc6012a
Bgstatic <bilgehangecici@gmail.com>
2020-09-06 14:06:29 +0300

    Main page of Spotify.

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import '../styles/Player.css';
+import Sidebar from './Sidebar';
+import Body from './Body';
+import Footer from './Footer';
 
 function Player({ spotify }) {
     return (
-        <div>
-            <h1>Welcome to Spotify</h1>
+        <div className="player">
+            <div className="player__body">
+                <Sidebar />
+                <Body />
+            </div>
+            <Footer />
         </div>
     )
 }
