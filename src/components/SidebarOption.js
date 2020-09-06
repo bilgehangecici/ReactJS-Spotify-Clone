2d0c9c26a0146169790c68b89631d4a64c5d532c
Bgstatic <bilgehangecici@gmail.com>
2020-09-06 15:34:25 +0300

    Added icons and title.

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -4,7 +4,8 @@ import '../styles/SidebarOption.css';
 function SidebarOption({ title, Icon }) {
     return (
         <div className="sidebarOption">
-          <p>{title}</p>
+            {Icon && <Icon className="sidebarOption__icon" />}
+            {Icon ? <h4> {title} </h4> : <p>{title}</p>}
         </div>
     )
 }
